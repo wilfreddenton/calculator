b51c3e7d949c1b6713f1e1913076392a8867418f
Wilfred Denton <dentonw3@gmail.com>
2014-10-20 13:19:29 -0400

    added some basic arithmetic functionality

--- scripts/controller.js
+++ scripts/controller.js
@@ -2,6 +2,7 @@
   // storage items
   var statement = "",
       rawStatement = "",
+      operator = '',
       memory = {};
 
   // initialize the calculator
@@ -9,24 +10,58 @@
 
   // functions
   function initializeCalculator() {
-    var btns = document.getElementsByClassName('btn');
-    var clickSound = document.getElementById('click-sound')
-    Array.prototype.forEach.call(btns, function(btn) {
-      btn.addEventListener('click', function(e) {
-        clickSound.play();
-        if (btn.className.indexOf('btn-operand') > -1) {
+    var btns = document.getElementsByClassName('btn'),
+        clickSound = document.getElementById('click-sound'),
+        displayValue = document.getElementById('display-raw'),
+        newOperand = true;
 
-        } else if (btn.className.indexOf('btn-operator') > -1) {
-          var usedOperator = document.getElementsByClassName('selected-operator');
-          if (usedOperator.length > 0) {
-            usedOperator[0].className = usedOperator[0].className.substring(0, usedOperator[0].className.indexOf('selected-operator'));
+    Array.prototype.forEach.call(btns, function(btn) {
+      btn.addEventListener('mousedown', function(e) {
+        clickSound.play();
+      });
+      if (btn.className.indexOf('btn-operand') > -1) {
+        btn.addEventListener('click', function(e) {
+          var operand = this.childNodes[1].innerHTML;
+          if (parseInt(displayValue.innerHTML) === 0 || newOperand) {
+            displayValue.innerHTML = operand
+            newOperand = false;
+          } else {
+            displayValue.innerHTML += operand;
           }
+          statement += operand;
+        });
+      } else if (btn.className.indexOf('btn-operator') > -1) {
+        btn.addEventListener('click', function(e) {
+          var usedOperator = document.getElementsByClassName('selected-operator');
+          resetOperators();
           btn.className = btn.className + ' selected-operator';
           console.log(btn.className);
-        } else if (btn.id == 'btn-equals') {
-          document.getElementsByClassNames
-        }
-      });
+
+          operator = this.childNodes[1].innerHTML;
+          switch (operator) {
+            case 'x':
+              operator = '*';
+              break;
+            case '÷':
+              operator = '/';
+              break;
+          }
+          statement += operator;
+          newOperand = true;
+        });
+      } else if (btn.id == 'btn-equals') {
+        btn.addEventListener('click', function(e) {
+          var ans = eval(statement);
+          displayValue.innerHTML = ans;
+          resetOperators();
+        });
+      }
     });
+  }
+  function resetOperators() {
+    var usedOperator = document.getElementsByClassName('selected-operator');
+    if (usedOperator.length > 0) {
+      usedOperator[0].className = usedOperator[0].className.substring(0, usedOperator[0].className.indexOf('selected-operator'));
+    }
   }
 })();
